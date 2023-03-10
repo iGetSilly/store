daf5ef79a7566f2859e076d1bffdd5834e888ee1
Leonardo Dutra <iuoyyp@gmail.com>
2023-03-10 09:58:32 -0300

    added the header and background img

--- src/components/Headers/index.js
+++ src/components/Headers/index.js
@@ -1,6 +1,14 @@
 function MainHeader() {
     return (
-        <h1>MainHeader</h1>
+        <div className="">
+            <nav className="navBar">
+                <a href="*"><img className="img-header-logo" alt="" src="img/logo-black.svg"></img></a>
+                <img className="img-header-menu" alt="" src="img/hamburger-icon.svg"></img>
+                <input type="text" placeholder="Pesquise aqui..." className="input-search"></input>
+                <a href="*"><img className="img-header-login" alt="" src="img/login-icon.svg"></img></a>
+                <a href="*"><img className="img-header-cart" alt="" src="img/Cart-icon.svg"></img></a>
+            </nav>
+        </div>
     )
 }
 
