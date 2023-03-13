a3f1d43b3c9077c97d7319a4903bb00e96fcdc4a
Leonardo Dutra <iuoyyp@gmail.com>
2023-03-12 22:57:15 -0300

    added the benefits to the header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import MainHeader from './components/Headers';
 import { motion } from 'framer-motion'
 import {useState, useEffect, useRef} from 'react'
 
-// import images
+// import images carousel
 
 import boxIcon from './images/carrousel/Box icon.svg'
 import cashIcon from './images/carrousel/Cash Icon.svg'
@@ -10,6 +10,13 @@ import celIcon from './images/carrousel/Cel icon.svg'
 import clockIcon from './images/carrousel/clock icon.svg'
 import headphoneIcon from './images/carrousel/headphone icon.svg'
 import pcIcon from './images/carrousel/PC icon.svg'
+
+// import images benefits
+
+import boxIconBenefits from './images/benefits/boxIcon.svg'
+import truckIcon from './images/benefits/Caminhao Frete Gratis.svg'
+import cardIcon from './images/benefits/Card icon.svg'
+import tradeIcon from './images/benefits/Troca Gratis Icon.svg'
 
 
 import './css/App.css';
@@ -34,7 +41,7 @@ function App() {
         <MainHeader></MainHeader>
       </header>
       <main>
-      <motion.div ref={carousel} className='carousel' whileTap={{cursor: "grabbing"}}>
+      <motion.div  ref={carousel} className='carousel' whileTap={{cursor: "grabbing"}}>
           <motion.div 
             className='inner' 
             drag="x"
@@ -43,15 +50,19 @@ function App() {
             animate={{x: 0}}
             transition= {{ duration: 0.8}}
           >
-
               {images.map((img) => (
                 <motion.div className='item' key={img}>
                   <img src={img}></img>
                 </motion.div>
               ))}
-
           </motion.div>
         </motion.div>
+        <section className='benefits'>
+          <label><img src={truckIcon}></img>Frete Grátis</label>
+          <label><img src={tradeIcon}></img>troca grátis</label>
+          <label><img src={cardIcon}></img>Até 12x S/Juros</label>
+          <label><img src={boxIconBenefits}></img>Entrega rápida</label>
+        </section>
       </main>
     </div>
   ); 
